2e08864ffa67119c7b167b79781b617f82c60710
ORBITA\jljuan <nuevo.autor@example.com>
2012-12-20 00:00:36 +0100

    ORBITA\jljuan 20/12/2012

---
+++ CodigoFuente/x32/Orbita.Controles/Orbita.Controles.Comunicaciones/OClienteComs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Orbita.Controles.Comunicaciones
+{
+    public partial class OClienteComs : UserControl
+    {
+        public OClienteComs()
+        {
+            InitializeComponent();
+        }
+    }
+}
