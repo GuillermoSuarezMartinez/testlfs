dbc3738f07d1dd8bb40f15d6e0cd2c4d5d9ce157
ORBITA\aiba├▒ez <nuevo.autor@example.com>
2013-02-14 09:26:49 +0100

    Emspty

--- CodigoFuente/x32/Orbita.Controles/Orbita.Controles.VA/Dialogos/Componentes/ONotifyIcon.cs
+++ CodigoFuente/x32/Orbita.Controles/Orbita.Controles.VA/Dialogos/Componentes/ONotifyIcon.cs
@@ -14,16 +14,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.ComponentModel;
+using Orbita.VA.Comun;
 
 namespace Orbita.Controles.VA
 {
     /// <summary>
-    /// Especifica un componente que crea un icono en el área de notificación
+    /// Clase estática para la gestión de mensajes de notificación en la barra de tareas
     /// </summary>
-    public class ONotifyIcon : Component
+    public static class ONotificacionManager
     {
         #region Atributo(s)
-        private NotifyIcon: NotifyIcon
+        /// <summary>
+        /// Componente de notificación en la barra de tareas
+        /// </summary>
+        private static NotifyIcon NotifyIcon;
+
+        /// <summary>
+        /// Tiemo de visualización
+        /// </summary>
+        private static int TimeOut;
+	    #endregion
+
+        #region Método(s) público(s)
+        /// <summary>
+        /// Constructor de los campos de la clase
+        /// </summary>
+        public static void Constructor(NotifyIcon notifyIcon, int timeout)
+        {
+            NotifyIcon = notifyIcon;
+            TimeOut = timeout;
+        }
+
+        /// <summary>
+        /// Destruye los campos de la clase
+        /// </summary>
+        public static void Destructor()
+        {
+            NotifyIcon = null;
+        }
+
+        /// <summary>
+        /// Visualización de un mensaje en la barra de notificación
+        /// </summary>
+        /// <param name="mensaje"></param>
+        public static void Mensaje(string mensaje)
+        {
+            if (ODebug.IsWinForms() && (NotifyIcon != null))
+            {
+                NotifyIcon.ShowBalloonTip(TimeOut, Application.ProductName, mensaje, ToolTipIcon.Info);
+            }
+        }
         #endregion
     }
 }
