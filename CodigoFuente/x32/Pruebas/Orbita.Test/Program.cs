5518085276e76b53bec114f09900a2aae741e382
ORBITA\jljuan <nuevo.autor@example.com>
2013-02-20 11:31:19 +0100

    Emspty

---
+++ CodigoFuente/x32/Pruebas/Orbita.Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Orbita.Trazabilidad;
+
+namespace Orbita.Test
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string fichero = @"C:\TFS\OrbitaSoftware\Librerias\CodigoFuente\x32\Orbita\Orbita.Trazabilidad\Config\Ejemplos\Wrapper.config.xml";
+            LogManager.ConfiguracionLogger(fichero);
+            Application.Run(new Form1());
+        }
+    }
+}
