b3210ce48bfcd3eda0456b2ef1b158565b250878
ORBITA\jljuan <nuevo.autor@example.com>
2013-03-11 17:49:29 +0100

    Empty

---
+++ CodigoFuente/x32/Orbita.Controles/Orbita.Controles.Comunicaciones/Formularios/FrmConfigLogger.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace oVisorTCP
+{
+    partial class FrmConfigLogger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Infragistics.Win.Appearance appearance1 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance2 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance3 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance4 = new Infragistics.Win.Appearance();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmConfigLogger));
+            Infragistics.Win.Appearance appearance5 = new Infragistics.Win.Appearance();
+            this.lblPuerto = new Orbita.Controles.Comunes.OrbitaUltraLabel();
+            this.txtLogger = new Orbita.Controles.Comunes.OrbitaTextBox();
+            this.lblLogger = new Orbita.Controles.Comunes.OrbitaUltraLabel();
+            this.lblExtensionFicheros = new Orbita.Controles.Comunes.OrbitaUltraLabel();
+            this.txtCaracter = new Orbita.Controles.Comunes.OrbitaTextBox();
+            this.lblRutaBackup = new Orbita.Controles.Comunes.OrbitaUltraLabel();
+            this.txtExtensionFicheros = new Orbita.Controles.Comunes.OrbitaTextBox();
+            this.txtRutaBackup = new Orbita.Controles.Comunes.OrbitaTextBox();
+            this.lblCaracter = new Orbita.Controles.Comunes.OrbitaUltraLabel();
+            this.gbRemoting = new Orbita.Controles.Contenedores.OrbitaUltraGroupBox();
+            this.txtPuerto = new Orbita.Controles.Comunes.OrbitaTextBox();
+            this.gbBackup = new Orbita.Controles.Contenedores.OrbitaUltraGroupBox();
+            this.gbGeneral = new Orbita.Controles.Contenedores.OrbitaUltraGroupBox();
+            this.txtFilasError = new Orbita.Controles.Comunes.OrbitaTextBox();
+            this.lblFilasError = new Orbita.Controles.Comunes.OrbitaUltraLabel();
+            this.txtFilasTrazas = new Orbita.Controles.Comunes.OrbitaTextBox();
+            this.lblFilasTrazas = new Orbita.Controles.Comunes.OrbitaUltraLabel();
+            this.btnAceptar = new Orbita.Controles.Comunes.OrbitaUltraButton();
+            this.btnCancelar = new Orbita.Controles.Comunes.OrbitaUltraButton();
+            ((System.ComponentModel.ISupportInitialize)(this.txtLogger)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCaracter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtExtensionFicheros)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtRutaBackup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gbRemoting)).BeginInit();
+            this.gbRemoting.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPuerto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gbBackup)).BeginInit();
+            this.gbBackup.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gbGeneral)).BeginInit();
+            this.gbGeneral.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFilasError)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFilasTrazas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPuerto
+            // 
+            this.lblPuerto.AutoSize = true;
+            this.lblPuerto.Location = new System.Drawing.Point(16, 24);
+            this.lblPuerto.Name = "lblPuerto";
+            this.lblPuerto.Size = new System.Drawing.Size(41, 15);
+            this.lblPuerto.TabIndex = 0;
+            this.lblPuerto.Text = "Puerto:";
+            this.lblPuerto.UseMnemonic = false;
+            // 
+            // txtLogger
+            // 
+            this.txtLogger.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLogger.Location = new System.Drawing.Point(63, 47);
+            this.txtLogger.Name = "txtLogger";
+            this.txtLogger.Size = new System.Drawing.Size(100, 21);
+            this.txtLogger.TabIndex = 2;
+            // 
+            // lblLogger
+            // 
+            this.lblLogger.AutoSize = true;
+            this.lblLogger.Location = new System.Drawing.Point(14, 51);
+            this.lblLogger.Name = "lblLogger";
+            this.lblLogger.Size = new System.Drawing.Size(43, 15);
+            this.lblLogger.TabIndex = 3;
+            this.lblLogger.Text = "Logger:";
+            this.lblLogger.UseMnemonic = false;
+            // 
+            // lblExtensionFicheros
+            // 
+            this.lblExtensionFicheros.AutoSize = true;
+            this.lblExtensionFicheros.Location = new System.Drawing.Point(14, 24);
+            this.lblExtensionFicheros.Name = "lblExtensionFicheros";
+            this.lblExtensionFicheros.Size = new System.Drawing.Size(99, 15);
+            this.lblExtensionFicheros.TabIndex = 5;
+            this.lblExtensionFicheros.Text = "Extensión ficheros:";
+            this.lblExtensionFicheros.UseMnemonic = false;
+            // 
+            // txtCaracter
+            // 
+            this.txtCaracter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCaracter.Location = new System.Drawing.Point(327, 20);
+            this.txtCaracter.Name = "txtCaracter";
+            this.txtCaracter.Size = new System.Drawing.Size(47, 21);
+            this.txtCaracter.TabIndex = 4;
+            // 
+            // lblRutaBackup
+            // 
+            this.lblRutaBackup.AutoSize = true;
+            this.lblRutaBackup.Location = new System.Drawing.Point(39, 51);
+            this.lblRutaBackup.Name = "lblRutaBackup";
+            this.lblRutaBackup.Size = new System.Drawing.Size(74, 15);
+            this.lblRutaBackup.TabIndex = 7;
+            this.lblRutaBackup.Text = "Ruta ficheros:";
+            this.lblRutaBackup.UseMnemonic = false;
+            // 
+            // txtExtensionFicheros
+            // 
+            this.txtExtensionFicheros.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtExtensionFicheros.Location = new System.Drawing.Point(119, 20);
+            this.txtExtensionFicheros.Name = "txtExtensionFicheros";
+            this.txtExtensionFicheros.Size = new System.Drawing.Size(91, 21);
+            this.txtExtensionFicheros.TabIndex = 6;
+            // 
+            // txtRutaBackup
+            // 
+            this.txtRutaBackup.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRutaBackup.Location = new System.Drawing.Point(119, 47);
+            this.txtRutaBackup.Name = "txtRutaBackup";
+            this.txtRutaBackup.Size = new System.Drawing.Size(255, 21);
+            this.txtRutaBackup.TabIndex = 8;
+            // 
+            // lblCaracter
+            // 
+            this.lblCaracter.AutoSize = true;
+            this.lblCaracter.Location = new System.Drawing.Point(216, 24);
+            this.lblCaracter.Name = "lblCaracter";
+            this.lblCaracter.Size = new System.Drawing.Size(105, 15);
+            this.lblCaracter.TabIndex = 9;
+            this.lblCaracter.Text = "Carácter Separador:";
+            this.lblCaracter.UseMnemonic = false;
+            // 
+            // gbRemoting
+            // 
+            appearance1.BorderColor = System.Drawing.Color.DarkGray;
+            this.gbRemoting.ContentAreaAppearance = appearance1;
+            this.gbRemoting.Controls.Add(this.lblPuerto);
+            this.gbRemoting.Controls.Add(this.txtPuerto);
+            this.gbRemoting.Controls.Add(this.lblLogger);
+            this.gbRemoting.Controls.Add(this.txtLogger);
+            this.gbRemoting.Location = new System.Drawing.Point(12, 12);
+            this.gbRemoting.Name = "gbRemoting";
+            this.gbRemoting.OrbColorBorde = System.Drawing.Color.DarkGray;
+            this.gbRemoting.OrbColorCabecera = System.Drawing.Color.Empty;
+            this.gbRemoting.Size = new System.Drawing.Size(181, 86);
+            this.gbRemoting.TabIndex = 10;
+            this.gbRemoting.Text = "Logger remoting";
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPuerto.Location = new System.Drawing.Point(63, 20);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(100, 21);
+            this.txtPuerto.TabIndex = 1;
+            // 
+            // gbBackup
+            // 
+            appearance2.BorderColor = System.Drawing.Color.DarkGray;
+            this.gbBackup.ContentAreaAppearance = appearance2;
+            this.gbBackup.Controls.Add(this.lblRutaBackup);
+            this.gbBackup.Controls.Add(this.txtExtensionFicheros);
+            this.gbBackup.Controls.Add(this.txtCaracter);
+            this.gbBackup.Controls.Add(this.lblCaracter);
+            this.gbBackup.Controls.Add(this.lblExtensionFicheros);
+            this.gbBackup.Controls.Add(this.txtRutaBackup);
+            this.gbBackup.Location = new System.Drawing.Point(199, 12);
+            this.gbBackup.Name = "gbBackup";
+            this.gbBackup.OrbColorBorde = System.Drawing.Color.DarkGray;
+            this.gbBackup.OrbColorCabecera = System.Drawing.Color.Empty;
+            this.gbBackup.Size = new System.Drawing.Size(396, 86);
+            this.gbBackup.TabIndex = 11;
+            this.gbBackup.Text = "Backup logger";
+            // 
+            // gbGeneral
+            // 
+            appearance3.BorderColor = System.Drawing.Color.DarkGray;
+            this.gbGeneral.ContentAreaAppearance = appearance3;
+            this.gbGeneral.Controls.Add(this.txtFilasError);
+            this.gbGeneral.Controls.Add(this.lblFilasError);
+            this.gbGeneral.Controls.Add(this.txtFilasTrazas);
+            this.gbGeneral.Controls.Add(this.lblFilasTrazas);
+            this.gbGeneral.Location = new System.Drawing.Point(12, 104);
+            this.gbGeneral.Name = "gbGeneral";
+            this.gbGeneral.OrbColorBorde = System.Drawing.Color.DarkGray;
+            this.gbGeneral.OrbColorCabecera = System.Drawing.Color.Empty;
+            this.gbGeneral.Size = new System.Drawing.Size(583, 59);
+            this.gbGeneral.TabIndex = 12;
+            this.gbGeneral.Text = "General";
+            // 
+            // txtFilasError
+            // 
+            this.txtFilasError.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFilasError.Location = new System.Drawing.Point(389, 20);
+            this.txtFilasError.Name = "txtFilasError";
+            this.txtFilasError.Size = new System.Drawing.Size(75, 21);
+            this.txtFilasError.TabIndex = 11;
+            // 
+            // lblFilasError
+            // 
+            this.lblFilasError.AutoSize = true;
+            this.lblFilasError.Location = new System.Drawing.Point(245, 24);
+            this.lblFilasError.Name = "lblFilasError";
+            this.lblFilasError.Size = new System.Drawing.Size(138, 15);
+            this.lblFilasError.TabIndex = 10;
+            this.lblFilasError.Text = "Nº filas visualización error:";
+            this.lblFilasError.UseMnemonic = false;
+            // 
+            // txtFilasTrazas
+            // 
+            this.txtFilasTrazas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFilasTrazas.Location = new System.Drawing.Point(164, 20);
+            this.txtFilasTrazas.Name = "txtFilasTrazas";
+            this.txtFilasTrazas.Size = new System.Drawing.Size(75, 21);
+            this.txtFilasTrazas.TabIndex = 6;
+            // 
+            // lblFilasTrazas
+            // 
+            this.lblFilasTrazas.AutoSize = true;
+            this.lblFilasTrazas.Location = new System.Drawing.Point(14, 24);
+            this.lblFilasTrazas.Name = "lblFilasTrazas";
+            this.lblFilasTrazas.Size = new System.Drawing.Size(144, 15);
+            this.lblFilasTrazas.TabIndex = 5;
+            this.lblFilasTrazas.Text = "Nº filas visualización trazas:";
+            this.lblFilasTrazas.UseMnemonic = false;
+            // 
+            // btnAceptar
+            // 
+            appearance4.Image = ((object)(resources.GetObject("appearance4.Image")));
+            this.btnAceptar.Appearance = appearance4;
+            this.btnAceptar.ImageSize = new System.Drawing.Size(24, 24);
+            this.btnAceptar.Location = new System.Drawing.Point(393, 169);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Orbita.Estilo = Orbita.Controles.Comunes.EstiloBoton.Extragrande;
+            this.btnAceptar.Orbita.Tipo = Orbita.Controles.Shared.TipoBoton.Aceptar;
+            this.btnAceptar.Size = new System.Drawing.Size(98, 33);
+            this.btnAceptar.TabIndex = 14;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnCancelar
+            // 
+            appearance5.Image = ((object)(resources.GetObject("appearance5.Image")));
+            this.btnCancelar.Appearance = appearance5;
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.ImageSize = new System.Drawing.Size(24, 24);
+            this.btnCancelar.Location = new System.Drawing.Point(497, 169);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Orbita.Estilo = Orbita.Controles.Comunes.EstiloBoton.Extragrande;
+            this.btnCancelar.Orbita.Tipo = Orbita.Controles.Shared.TipoBoton.Cancelar;
+            this.btnCancelar.Size = new System.Drawing.Size(98, 33);
+            this.btnCancelar.TabIndex = 15;
+            this.btnCancelar.Text = "Cancelar";
+            // 
+            // FrmConfigLogger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancelar;
+            this.ClientSize = new System.Drawing.Size(610, 216);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.gbGeneral);
+            this.Controls.Add(this.gbBackup);
+            this.Controls.Add(this.gbRemoting);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmConfigLogger";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Configuración Visor";
+            this.Load += new System.EventHandler(this.FrmConfigLogger_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtLogger)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtCaracter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtExtensionFicheros)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtRutaBackup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gbRemoting)).EndInit();
+            this.gbRemoting.ResumeLayout(false);
+            this.gbRemoting.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPuerto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gbBackup)).EndInit();
+            this.gbBackup.ResumeLayout(false);
+            this.gbBackup.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gbGeneral)).EndInit();
+            this.gbGeneral.ResumeLayout(false);
+            this.gbGeneral.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFilasError)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFilasTrazas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Orbita.Controles.Comunes.OrbitaUltraLabel lblPuerto;
+        private Orbita.Controles.Comunes.OrbitaTextBox txtLogger;
+        private Orbita.Controles.Comunes.OrbitaUltraLabel lblLogger;
+        private Orbita.Controles.Comunes.OrbitaUltraLabel lblExtensionFicheros;
+        private Orbita.Controles.Comunes.OrbitaTextBox txtCaracter;
+        private Orbita.Controles.Comunes.OrbitaUltraLabel lblRutaBackup;
+        private Orbita.Controles.Comunes.OrbitaTextBox txtExtensionFicheros;
+        private Orbita.Controles.Comunes.OrbitaTextBox txtRutaBackup;
+        private Orbita.Controles.Comunes.OrbitaUltraLabel lblCaracter;
+        private Orbita.Controles.Contenedores.OrbitaUltraGroupBox gbRemoting;
+        private Orbita.Controles.Contenedores.OrbitaUltraGroupBox gbBackup;
+        private Orbita.Controles.Contenedores.OrbitaUltraGroupBox gbGeneral;
+        private Orbita.Controles.Comunes.OrbitaTextBox txtFilasError;
+        private Orbita.Controles.Comunes.OrbitaUltraLabel lblFilasError;
+        private Orbita.Controles.Comunes.OrbitaTextBox txtFilasTrazas;
+        private Orbita.Controles.Comunes.OrbitaUltraLabel lblFilasTrazas;
+        private Orbita.Controles.Comunes.OrbitaUltraButton btnAceptar;
+        private Orbita.Controles.Comunes.OrbitaUltraButton btnCancelar;
+        private Orbita.Controles.Comunes.OrbitaTextBox txtPuerto;
+    }
+}
---
+++ CodigoFuente/x32/Orbita.Controles/Orbita.Controles.Comunicaciones/Formularios/FrmDetalle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+using Orbita.Trazabilidad;
+
+
+namespace oVisorTCP
+{
+    public partial class FrmDetalle : Form
+    {
+        #region Atributo(s)
+        /// <summary>
+        /// Fila a mostrar.
+        /// </summary>
+        Infragistics.Win.UltraWinGrid.UltraGridRow _fila;
+        #endregion
+
+        #region Constructor(es)
+        /// <summary>
+        /// Constructor del formulario.
+        /// </summary>
+        /// <param name="fila">Fila del grid con datos.</param>
+        public FrmDetalle(Infragistics.Win.UltraWinGrid.UltraGridRow fila)
+        {
+            InitializeComponent();
+            this._fila = fila;
+            this.Incializar(fila);
+        }
+        #endregion
+
+        #region Métodos Privado(s)
+        /// <summary>
+        /// Inicializa el formulario.
+        /// </summary>
+        /// <param name="fila">Fila del grid con datos.</param>
+        void Incializar(Infragistics.Win.UltraWinGrid.UltraGridRow fila)
+        {
+            txtNivel.Text = fila.Cells["Nivel"].Value.ToString();
+            txtFecha.Text = fila.Cells["Fecha"].Value.ToString();
+            txtMensaje.Text = fila.Cells["Mensaje"].Value.ToString();
+
+            //Se carga el PictureBox
+            switch ((NivelLog)Enum.Parse(typeof(NivelLog), fila.Cells["Nivel"].Value.ToString(), true))
+            {
+                case NivelLog.Debug:
+                case NivelLog.Info:
+                    if (!String.IsNullOrEmpty(Convert.ToString(fila.Cells["Mensaje"].Value)))
+                    {
+                        if (Convert.ToString(fila.Cells["Mensaje"].Value).Length >= 9)
+                        {
+                            if (Convert.ToString(fila.Cells["Mensaje"].Value).Substring(1, 9) == "KeepAlive")
+                                picNivel.Image = Properties.Resources.MensajeKeepAlive;
+                            else
+                                picNivel.Image = Properties.Resources.MensajeInformacion;
+                        }
+                    }
+                    break;
+                case NivelLog.Warn:
+                    picNivel.Image = Properties.Resources.MensajeAdvertencia;
+                    break;
+                case NivelLog.Error:
+                    picNivel.Image = Properties.Resources.MensajeError;
+                    break;
+                case NivelLog.Fatal:
+                    picNivel.Image = Properties.Resources.MensajeFatal;
+                    break;
+                default:
+                    picNivel.Image = Properties.Resources.MensajeInformacion;
+                    break;
+            }
+        }
+        #endregion
+    }
+}
