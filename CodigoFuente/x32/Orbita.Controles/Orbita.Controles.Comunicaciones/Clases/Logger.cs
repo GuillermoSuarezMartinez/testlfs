8c289503ed7c7f63ce0b99ecaf871757182c2f61
ORBITA\jljuan <nuevo.autor@example.com>
2013-03-11 16:07:30 +0100

    Empty

---
+++ CodigoFuente/x32/Orbita.Controles/Orbita.Controles.Comunicaciones/Clases/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Orbita.Trazabilidad;
+
+namespace oVisorTCP
+{
+    /// <summary>
+    /// Clase para la recepción de logs por remoting.
+    /// </summary>
+    public class Logger : MarshalByRefObject, IRemotingLogger
+    {
+        #region Método(s) Público(s)
+        /// <summary>
+        /// Entrada de logs.
+        /// </summary>
+        /// <param name="mensajes">Listado de mensajes.</param>
+        public void Log(ItemLog[] mensajes)
+        {
+            oVisorTCP visor = oVisorTCP.Visor;
+            if (visor != null)
+            {
+                if (mensajes != null)
+                {
+                    foreach (ItemLog mensaje in mensajes)
+                    {
+                        oVisorTCP.Visor.AddLinea(mensaje);
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
