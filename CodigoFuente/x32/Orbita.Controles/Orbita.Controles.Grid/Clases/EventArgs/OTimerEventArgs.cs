b2e1410a121c3452c05a7e4f329b56adc9ebf115
ORBITA\crodriguez <nuevo.autor@example.com>
2013-03-12 16:45:15 +0100

    Empty

---
+++ CodigoFuente/x32/Orbita.Controles/Orbita.Controles.Grid/Clases/EventArgs/OTimerEventArgs.cs
@@ -0,0 +1,35 @@
+﻿//***********************************************************************
+// Assembly         : Orbita.Controles.Grid
+// Author           : crodriguez
+// Created          : 19-01-2012
+//
+// Last Modified By : crodriguez
+// Last Modified On : 19-01-2012
+// Description      : 
+//
+// Copyright        : (c) Orbita Ingenieria. All rights reserved.
+//***********************************************************************
+namespace Orbita.Controles.Grid
+{
+    public class OTimerEventArgs : System.EventArgs
+    {
+        #region Atributos privados
+        int intervalo;
+        #endregion
+
+        #region Constructores
+        public OTimerEventArgs(int intervalo)
+        {
+            this.intervalo = intervalo;
+        }
+        #endregion
+
+        #region Propiedades
+        public int Intervalo
+        {
+            get { return this.intervalo; }
+            set { this.intervalo = value; }
+        }
+        #endregion
+    }
+}
