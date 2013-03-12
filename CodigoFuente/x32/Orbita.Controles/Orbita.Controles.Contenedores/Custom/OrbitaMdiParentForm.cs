79fcb30cf0b5258d9b4db986992ec4f6217e10d9
ORBITA\crodriguez <nuevo.autor@example.com>
2013-03-12 16:53:34 +0100

    Empty

---
+++ CodigoFuente/x32/Orbita.Controles/Orbita.Controles.Contenedores/Custom/OrbitaMdiParentForm.cs
@@ -0,0 +1,66 @@
+//***********************************************************************
+// Assembly         : Orbita.Controles.Contenedores
+// Author           : crodriguez
+// Created          : 19-01-2012
+//
+// Last Modified By : crodriguez
+// Last Modified On : 19-01-2012
+// Description      : 
+//
+// Copyright        : (c) Orbita Ingenieria. All rights reserved.
+//***********************************************************************
+namespace Orbita.Controles.Contenedores
+{
+    public partial class OrbitaMdiParentForm : System.Windows.Forms.Form
+    {
+        #region Nueva definición
+        public class ControlNuevaDefinicion : OMdiParentForm
+        {
+            public ControlNuevaDefinicion(OrbitaMdiParentForm sender)
+                : base(sender) { }
+        }
+        #endregion
+
+        #region Atributos
+        ControlNuevaDefinicion definicion;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Inicializar una nueva instancia de la clase Orbita.Controles.Contenedores.OrbitaMdiParentForm.
+        /// </summary>
+        public OrbitaMdiParentForm()
+            : base()
+        {
+            InitializeComponent();
+            InitializeAttributes();
+            InitializeProperties();
+        }
+        #endregion
+
+        #region Propiedades
+        [System.ComponentModel.Category("Gestión de controles")]
+        [System.ComponentModel.DesignerSerializationVisibility(System.ComponentModel.DesignerSerializationVisibility.Content)]
+        public ControlNuevaDefinicion OI
+        {
+            get { return this.definicion; }
+            set { this.definicion = value; }
+        }
+        #endregion
+
+        #region Métodos privados
+        void InitializeAttributes()
+        {
+            if (this.definicion == null)
+            {
+                this.definicion = new ControlNuevaDefinicion(this);
+            }
+        }
+        void InitializeProperties()
+        {
+            this.toolTip.Active = Configuracion.DefectoVerToolTips;
+            this.OI.NumeroMaximoFormulariosAbiertos = Configuracion.DefectoNumeroMaximoFormulariosAbiertos;
+        }
+        #endregion
+    }
+}
