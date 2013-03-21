dd0402dadc0f582725ade715bc4540dc7ec872e2
ORBITA\aiba├▒ez <nuevo.autor@example.com>
2013-03-21 18:06:30 +0100

    Empty

--- CodigoFuente/x32/Orbita.VA/Orbita.VA.Comun/Properties/AssemblyInfo.cs
+++ CodigoFuente/x32/Orbita.VA/Orbita.VA.Comun/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("13.78.8.1")]
-[assembly: AssemblyFileVersion("13.78.8.1")]
+[assembly: AssemblyVersion("13.80.8.1")]
+[assembly: AssemblyFileVersion("13.80.8.1")]
