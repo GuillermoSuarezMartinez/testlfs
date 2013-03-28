6a5f2084b7fdee130e1df18d8a44fcddd4a483fe
ORBITA\crodriguez <nuevo.autor@example.com>
2013-03-28 09:13:19 +0100

    Empty

--- CodigoFuente/x32/Orbita/Orbita.Trazabilidad/Repositorio/LogManager.cs
+++ CodigoFuente/x32/Orbita/Orbita.Trazabilidad/Repositorio/LogManager.cs
@@ -13,14 +13,13 @@
 //
 // Copyright        : (c) Orbita Ingenieria. All rights reserved.
 //***********************************************************************
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Collections;
-
 namespace Orbita.Trazabilidad
 {
     /// <summary>
-    /// LogManager.
+    /// Log manager.
     /// </summary>
     public static class LogManager
     {
