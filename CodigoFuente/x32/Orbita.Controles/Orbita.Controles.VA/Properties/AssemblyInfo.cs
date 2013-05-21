2546ee56f31ea12579e50b386dc763a23320b7f4
ORBITA\aiba├▒ez <nuevo.autor@example.com>
2013-05-21 10:00:11 +0200

    Empty

--- CodigoFuente/x32/Orbita.Controles/Orbita.Controles.VA/Properties/AssemblyInfo.cs
+++ CodigoFuente/x32/Orbita.Controles/Orbita.Controles.VA/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.140.8.1")]
-[assembly: AssemblyFileVersion("13.140.8.1")]
+[assembly: AssemblyVersion("13.141.8.1")]
+[assembly: AssemblyFileVersion("13.141.8.1")]
