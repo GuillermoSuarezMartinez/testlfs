61f2eb3e17ea821f0cd095535286953c1368f600
ORBITA\crodriguez <nuevo.autor@example.com>
2013-06-26 15:33:23 +0200

    Empty

--- CodigoFuente/x32/Orbita/Orbita.Xml/Core/OXml.cs
+++ CodigoFuente/x32/Orbita/Orbita.Xml/Core/OXml.cs
@@ -23,6 +23,7 @@ using System.Globalization;
 using System.IO;
 using System.Xml;
 using System.Xml.XPath;
+using System.Collections.Generic;
 namespace Orbita.Xml
 {
     /// <summary>
@@ -42,6 +43,22 @@ namespace Orbita.Xml
         /// Variable para escribir el XML
         /// </summary>
         XmlTextWriter escritorXml;
+        /// <summary>
+        /// Se ha ejecutado el dispose
+        /// </summary>
+        bool _disposed = false;
+        /// <summary>
+        /// String con xml almacenado
+        /// </summary>
+        string _xmlDato;
+        /// <summary>
+        /// Propiedades del reader
+        /// </summary>
+        XmlReaderSettings _settings;
+        /// <summary>
+        /// Listado de Subnodos del nodo principal del objeto
+        /// </summary>
+        Dictionary<string, string> _subNodos;
         #endregion Atributos
 
         #region Constructor
@@ -53,6 +70,61 @@ namespace Orbita.Xml
             // Para escritura.
             this.swXml = new StringWriter(CultureInfo.CurrentCulture);
             this.escritorXml = new XmlTextWriter(this.swXml);
+            this._xmlDato = string.Empty;
+            this._subNodos = new Dictionary<string, string>();
+            //asignamos las propiedades del reader para procesar el xml
+            _settings = new XmlReaderSettings();
+            //se ignoran los comentarios
+            _settings.IgnoreComments = true;
+            //se ignoran los espacios en blanco
+            _settings.IgnoreWhitespace = true;
+            _settings.ValidationType = ValidationType.None;
+            _settings.IgnoreProcessingInstructions = true;
+            _settings.CheckCharacters = false;
+        }
+        /// <summary>
+        /// Constructor con parámetros
+        /// </summary>
+        /// <param name="_xmlDato">xml a procesar</param>
+        public OXml(string _xmlDato)
+        {
+            this.swXml = new StringWriter(CultureInfo.CurrentCulture);
+            this.escritorXml = new XmlTextWriter(this.swXml);
+            this._xmlDato = _xmlDato;
+            this._subNodos = new Dictionary<string, string>();
+            //asignamos las propiedades del reader para procesar el xml
+            _settings = new XmlReaderSettings();
+            //se ignoran los comentarios
+            _settings.IgnoreComments = true;
+            //se ignoran los espacios en blanco
+            _settings.IgnoreWhitespace = true;
+            _settings.ValidationType = ValidationType.None;
+            _settings.IgnoreProcessingInstructions = true;
+            _settings.CheckCharacters = false;
+        }
+        /// <summary>
+        /// Constructor con parámetros
+        /// </summary>
+        /// <param name="_xmlDato">xml a procesar</param>
+        /// <param name="carga" > si se va ha realizar recarga </param>
+        public OXml(string _xmlDato, bool carga)
+        {
+            this.swXml = new StringWriter(CultureInfo.CurrentCulture);
+            this.escritorXml = new XmlTextWriter(this.swXml);
+            this._subNodos = new Dictionary<string, string>();
+            //asignamos las propiedades del reader para procesar el xml
+            _settings = new XmlReaderSettings();
+            //se ignoran los comentarios
+            _settings.IgnoreComments = true;
+            //se ignoran los espacios en blanco
+            _settings.IgnoreWhitespace = true;
+            _settings.ValidationType = ValidationType.None;
+            _settings.IgnoreProcessingInstructions = true;
+            _settings.CheckCharacters = false;
+            if (carga)
+                LoadXml(_xmlDato);
+            else
+                this._xmlDato = _xmlDato;
         }
         #endregion
 
@@ -72,6 +144,20 @@ namespace Orbita.Xml
         {
             get { return this.escritorXml; }
             set { this.escritorXml = value; }
+        }
+        /// <summary>
+        /// Devuelve el número de hijos del arbol
+        /// </summary>
+        public int NumChildren
+        {
+            get { return NumeroHijos(); }
+        }
+        /// <summary>
+        /// Devuelve el string del xml
+        /// </summary>
+        public string XmlDato
+        {
+            get { return this._xmlDato; }
         }
         #endregion Propiedades
 
@@ -314,6 +400,191 @@ namespace Orbita.Xml
             sr.Close();
             return (xpathDoc);
         }
+        /// <summary>
+        /// Carga el xml, realiza acciones iniciales
+        /// </summary>
+        /// <param name="datoXML">dato xml como string</param>
+        /// <returns>carga correcta</returns>
+        public bool LoadXml(string datoXML)
+        {
+            try
+            {
+                _subNodos.Clear();
+                this._xmlDato = datoXML;
+                //cuenta el número de hijos, se obtiene su nombre y el xml asociado
+                using (XmlReader reader = XmlReader.Create(new StringReader(datoXML), _settings))
+                {
+                    int profundidadHijo = reader.Depth + 1;
+                    //la variable iteraciones se utiliza por si hay subnodos con el mismo nombre (para diferenciarlos en el diccionario)
+                    int iteraciones = 0;
+                    //Se inicia el recorrido del xml, en la carga inicial se gestionan los subnodos
+                    reader.MoveToContent();
+                    if (reader.Read())
+                    {
+                        while (true)
+                        {
+                            if (reader.NodeType == XmlNodeType.Element)
+                            {
+                                //si es un hijo
+                                if (reader.Depth == profundidadHijo)
+                                {
+                                    iteraciones++;
+                                    string key = string.Empty;
+                                    //se añade al diccionario y se salta al siguiente hijo
+                                    //comprueba si existe un nodo en el diccionario con el mismo nombre para añadir índice
+                                    //el indíce es interno, no afecta a las búsquedas
+                                    if (_subNodos.TryGetValue(reader.Name, out key))
+                                        _subNodos.Add(reader.Name + "[" + iteraciones.ToString() + "]", reader.ReadOuterXml());
+                                    else
+                                        _subNodos.Add(reader.Name, reader.ReadOuterXml());
+                                }
+                                else//si se ha profundizado mas de lo necesario salimos del bucle
+                                {
+                                    break;
+                                }
+                            }
+                            else if (!reader.Read())//si no hay mas elementos a leer salimos del bucle
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    reader.Close();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// Obtiene un nodo a partir de una ruta pasada como parámetro, separadas las etiquetas por |.
+        /// </summary>
+        /// <param name="rutaPathEtiquetas">Relativa a las etiquetas tipo "Presupuesto|Partida|IdPartida", sin añadimos "Presupuesto|Partida[2]|IdPartida iriamos al 3 elemento</param>
+        /// <returns>valor del nodo a buscar</returns>
+        public string GetNodo(string rutaPathEtiquetas)
+        {
+            int i = 0;
+            //Array con la ruta del nodo a buscar, cada etiqúeta separada por símbolo '|'
+            string[] r = rutaPathEtiquetas.Split('|');
+            string valor = string.Empty;
+            //indica índice del nodo (si hay varios con el mismo nombre buscara el nodo numero...)
+            int nodo = 0;
+
+            using (XmlReader reader = XmlReader.Create(new StringReader(this._xmlDato), _settings))
+            {
+                reader.MoveToContent();
+
+                //se recorre el array buscando los elementos (se avanza en el reader cuando es necesario)
+                while (i < r.Length)
+                {
+                    //se comprueba si el nodo tiene índice
+                    if (r[i].Contains("["))
+                    {
+                        nodo = Convert.ToInt32(r[i].Substring(r[i].IndexOf("[") + 1, 1));
+                        r[i] = r[i].Substring(0, r[i].IndexOf("["));
+                    }
+                    //se busca el siguiente nodo con nombre de elemento en array
+                    if (reader.ReadToFollowing(r[i]))
+                    {
+                        if (reader.NodeType == XmlNodeType.Element)
+                        {
+                            //si es el último elemento se obtiene el valor
+                            if (i == r.Length - 1)
+                            {
+                                valor = reader.ReadString();
+                                break;
+                            }//si no es el último elemento se avanza al siguiente
+                            else if (nodo == 0)
+                            {
+                                reader.MoveToElement();
+                                i++;
+                            }
+                            else//si el nodo tiene índice (ej. [2]) se avanza al siguiente nodo con mismo nombre (i no se incrementa)
+                                nodo--;
+                        }
+                    }
+                    else
+                        break;
+                }
+
+                reader.Close();
+            }
+
+            return valor != string.Empty ? valor : null;
+        }
+        /// <summary>
+        /// Comprueba si el xml contiene un nodo con un nombre dado
+        /// </summary>
+        /// <param name="etiqueta">nombre del nodo</param>
+        /// <returns>nodo existe</returns>
+        public bool ContieneNodo(string etiqueta)
+        {
+            string key = string.Empty;
+            //se busca la etiqueta en el diccionario de subnodos
+            if (_subNodos.TryGetValue(etiqueta, out key))
+                return true;
+            else
+                return false;
+        }
+        /// <summary>
+        /// Comprueba si el xml contiene un atributo con un nombre dado
+        /// </summary>
+        /// <param name="valor">Nombre del Atributo</param>
+        /// <returns>atributo existe</returns>
+        public bool ContieneAtributo(string valor)
+        {
+            string atributo = string.Empty;
+            using (XmlTextReader reader = new XmlTextReader(new StringReader(this._xmlDato)))
+            {
+                reader.MoveToContent();
+                bool att = reader.HasAttributes;
+                atributo = reader.GetAttribute(valor);
+                reader.Close();
+            }
+            return atributo != string.Empty && atributo != null ? true : false;
+        }
+        /// <summary>
+        /// Calcula el número de hijos de un nodo
+        /// </summary>
+        /// <returns>numero de hijos</returns>
+        public int NumeroHijos()
+        {
+            return _subNodos.Count;
+        }
+        /// <summary>
+        /// Obtiene el subarbol con el indice pasado por parámetro (desde nodo raiz).
+        /// </summary>
+        /// <param name="id">indice del subarbol</param>
+        /// <returns></returns>
+        public string ObtenerNodoHijo(int id)
+        {
+            string resultado = null;
+            if (_subNodos.Count >= id + 1)
+            {
+                //Como el diccionario no utiliza índices estos se gestionan desde un array
+                string[] keys = new string[_subNodos.Count];
+                _subNodos.Keys.CopyTo(keys, 0);
+                resultado = _subNodos[keys[id]].ToString();
+            }
+            return resultado;
+        }
+        /// <summary>
+        /// Obtiene el subarbol con el nombre pasado por parámetro(desde nodo raiz).
+        /// </summary>
+        /// <param name="etiqueta">indice del subarbol</param>
+        /// <returns></returns>
+        public string ObtenerNodoHijo(string etiqueta)
+        {
+            string resultado = null; string key = string.Empty;
+            //se busca el subnodo por nombre como key
+            if (_subNodos.TryGetValue(etiqueta, out key))
+            {
+                resultado = _subNodos[etiqueta].ToString();
+            }
+            return resultado;
+        }
         #endregion
 
         #region IDisposable Members
@@ -322,6 +593,7 @@ namespace Orbita.Xml
         /// </summary>
         public void Dispose()
         {
+            this.Dispose(true);
             if (this.escritorXml != null)
             {
                 this.escritorXml.Close();
@@ -333,6 +605,33 @@ namespace Orbita.Xml
                 this.swXml.Dispose();
                 this.swXml = null;
             }
+            GC.SuppressFinalize(this);
+        }
+        /// <summary>
+        /// Finalizador de la clase, llama a dispose
+        /// </summary>
+        ~OXml()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Elimina los recursos utilizados, se comprueba si se ha llamado para no realizarlo 2 veces
+        /// </summary>
+        /// <param name="disposing"></param>
+        void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    this._xmlDato = string.Empty;
+                    this._xmlDato = null;
+                    this._subNodos.Clear();
+                    this._subNodos = null;
+                }
+            }
+            _disposed = true;
         }
         #endregion
     }
