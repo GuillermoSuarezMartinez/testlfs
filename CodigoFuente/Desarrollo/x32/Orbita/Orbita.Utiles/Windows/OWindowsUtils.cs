c2fc71ae345c0bfc42c0b7d69d535640b2506d99
ORBITA\aiba├▒ez <nuevo.autor@example.com>
2014-02-10 17:04:49 +0100

    Versi├│n utilizada en las lectoras de sombras

--- CodigoFuente/Desarrollo/x32/Orbita/Orbita.Utiles/Windows/OWindowsUtils.cs
+++ CodigoFuente/Desarrollo/x32/Orbita/Orbita.Utiles/Windows/OWindowsUtils.cs
@@ -71,24 +71,15 @@ namespace Orbita.Utiles
         /// <summary>
         /// memcpy - copy a block of memery
         /// </summary>
-        /// <param name="dst">Puntero destino</param>
-        /// <param name="src">Puntero origen</param>
-        /// <param name="count">Tamaño en bytes de la copia</param>
+        /// <param name="dst"></param>
+        /// <param name="src"></param>
+        /// <param name="count"></param>
         /// <returns></returns>
         [DllImport("ntdll.dll")]
         public static extern int memcpy(
             int dst,
             int src,
             int count);
-
-        /// <summary>
-        /// Copia de memoria
-        /// </summary>
-        /// <param name="dest">Puntero destino</param>
-        /// <param name="src">Puntero origen</param>
-        /// <param name="count">Tamaño en bytes de la copia</param>
-        [DllImport("kernel32.dll", EntryPoint = "CopyMemory", SetLastError = false)]
-        public static extern void CopyMemory(IntPtr dest, IntPtr src, uint count);
         #endregion
 
         #region Métodos de red
