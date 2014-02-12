187b8849b8d0e6251689456796ae1cbd9a4fee61
ORBITA\crodriguez <nuevo.autor@example.com>
2014-02-12 13:20:46 +0100

    Empty

--- CodigoFuente/Desarrollo/x32/Comun/Orbita/Ofuscar/OfuscarOrbita.cmd
+++ CodigoFuente/Desarrollo/x32/Comun/Orbita/Ofuscar/OfuscarOrbita.cmd
@@ -6,7 +6,7 @@ IF %resp% == n GOTO Comunicaciones
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.BBDD.dll /q/f
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.BBDD.xml /q/f
 "%PROGRAMFILES%\Red Gate\SmartAssembly 6\SmartAssembly.com" /build .\Orbita.BBDD32.saproj
-verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.BBDD.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2013"
+verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.BBDD.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2014"
 xcopy ..\Orbita.BBDD.xml C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita
 
 :Comunicaciones
@@ -18,7 +18,7 @@ IF %resp% == n GOTO Winsock
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Comunicaciones.dll /q/f
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Comunicaciones.xml /q/f
 "%PROGRAMFILES%\Red Gate\SmartAssembly 6\SmartAssembly.com" /build .\Orbita.Comunicaciones32.saproj
-verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Comunicaciones.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2013"
+verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Comunicaciones.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2014"
 xcopy ..\Orbita.Comunicaciones.xml C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita
 
 :Winsock
@@ -30,7 +30,7 @@ IF %resp% == n GOTO Trazabilidad
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Winsock.dll /q/f
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Winsock.xml /q/f
 "%PROGRAMFILES%\Red Gate\SmartAssembly 6\SmartAssembly.com" /build .\Orbita.Winsock32.saproj
-verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Winsock.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2013"
+verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Winsock.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2014"
 xcopy ..\Orbita.Winsock.xml C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita
 
 :Trazabilidad
@@ -42,7 +42,7 @@ IF %resp% == n GOTO Utiles
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Trazabilidad.dll /q/f
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Trazabilidad.xml /q/f
 "%PROGRAMFILES%\Red Gate\SmartAssembly 6\SmartAssembly.com" /build .\Orbita.Trazabilidad32.saproj
-verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Trazabilidad.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2013"
+verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Trazabilidad.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2014"
 xcopy ..\Orbita.Trazabilidad.xml C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita
 
 :Utiles
@@ -54,7 +54,7 @@ IF %resp% == n GOTO MS
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Utiles.dll /q/f
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Utiles.xml /q/f
 "%PROGRAMFILES%\Red Gate\SmartAssembly 6\SmartAssembly.com" /build .\Orbita.Utiles32.saproj
-verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Utiles.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2013"
+verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Utiles.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2014"
 xcopy ..\Orbita.Utiles.xml C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita
 
 :MS
@@ -66,7 +66,7 @@ IF %resp% == n GOTO Xml
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.MS.dll /q/f
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.MS.xml /q/f
 "%PROGRAMFILES%\Red Gate\SmartAssembly 6\SmartAssembly.com" /build .\Orbita.MS32.saproj
-verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.MS.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2013"
+verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.MS.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2014"
 xcopy ..\Orbita.MS.xml C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita
 
 :Xml
@@ -78,7 +78,7 @@ IF %resp% == n GOTO Compresion
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Xml.dll /q/f
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Xml.xml /q/f
 "%PROGRAMFILES%\Red Gate\SmartAssembly 6\SmartAssembly.com" /build .\Orbita.Xml32.saproj
-verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Xml.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2013"
+verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Xml.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2014"
 xcopy ..\Orbita.Xml.xml C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita
 
 :Compresion
@@ -90,7 +90,7 @@ IF %resp% == n GOTO teamfundation
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Compresion.dll /q/f
 del C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Compresion.xml /q/f
 "%PROGRAMFILES%\Red Gate\SmartAssembly 6\SmartAssembly.com" /build .\Orbita.Compresion32.saproj
-verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Compresion.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2013"
+verpatch C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita\Orbita.Compresion.dll /s company "Orbita Software" /s copyright "Copyright © Orbita Ingenieria SW 2014"
 xcopy ..\Orbita.Compresion.xml C:\TFS\Orbita\General\Dlls\Desarrollo\x32\Orbita
 
 :teamfundation
